1b6aff5cc3d4fd517fef1f76b64c935d0982cddf
Sreejith.K.R <Sreejith.K.R@cognizant.com>
2014-09-24 14:50:03 +0530

    Initial Commit

---
+++ GitExplore/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitExplore
+//
+//  Created by developer on 24/09/14.
+//  Copyright (c) 2014 Cognizant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitExplore/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitExplore
+//
+//  Created by developer on 24/09/14.
+//  Copyright (c) 2014 Cognizant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
